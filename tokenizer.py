f8c945fc770b986b8d007768d3e5ac4fac8acf16
Asish <iamasish3001@gmail.com>
2019-12-23 18:49:14 +0530

    few more lines added

--- tokenizer.py
+++ tokenizer.py
@@ -1,2 +1,9 @@
 #simple program of tokenization 
+
+#importing nltk library
 import nltk
+text = "this is Asish's text, isn't it? "
+
+tokenizer = nltk.tokenize.WhitespaceTokenizer()
+whitespace_text = tokenizer.tokenize(text)
+print(whitespace_text)
