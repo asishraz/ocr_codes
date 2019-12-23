98017d8533b25666c7d13c3ddaf1e2e0bdd16a50
Asish <iamasish3001@gmail.com>
2019-12-23 18:59:56 +0530

    added few lines of tokenization code

--- tokenizer.py
+++ tokenizer.py
@@ -4,6 +4,26 @@
 import nltk
 text = "this is Asish's text, isn't it? "
 
+
+'''these all are first step of tokenization 
+1. white space tokenization
+'''
 tokenizer = nltk.tokenize.WhitespaceTokenizer()
 whitespace_text = tokenizer.tokenize(text)
 print(whitespace_text)
+
+'''
+2. punctuation tokenization
+'''
+tokenizer = nltk.tokenize.WordPunctTokenizer()
+punctuation_text = tokenizer.tokenize(text)
+print(punctuation_text)
+
+'''
+3. grammar tokenization
+'''
+
+tokenizer = nltk.tokenize.TreebankWordTokenizer()
+grammar_text = tokenizer.tokenize(text)
+print(grammar_text)
+
