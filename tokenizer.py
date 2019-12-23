1937268f7c7e00dc0cbcb25eb32a08a467115ebf
Asish <iamasish3001@gmail.com>
2019-12-23 19:10:29 +0530

    ending lines added

--- tokenizer.py
+++ tokenizer.py
@@ -8,22 +8,28 @@ text = "this is Asish's text, isn't it? "
 '''these all are first step of tokenization 
 1. white space tokenization
 '''
-tokenizer = nltk.tokenize.WhitespaceTokenizer()
-whitespace_text = tokenizer.tokenize(text)
+tokenizer1 = nltk.tokenize.WhitespaceTokenizer()
+whitespace_text = tokenizer1.tokenize(text)
+print("white space tokenization: ")
 print(whitespace_text)
+print()
 
 '''
 2. punctuation tokenization
 '''
-tokenizer = nltk.tokenize.WordPunctTokenizer()
-punctuation_text = tokenizer.tokenize(text)
+tokenizer2 = nltk.tokenize.WordPunctTokenizer()
+punctuation_text = tokenizer2.tokenize(text)
+print("punctuation tokenization: ")
 print(punctuation_text)
+print()
 
 '''
 3. grammar tokenization
 '''
 
-tokenizer = nltk.tokenize.TreebankWordTokenizer()
-grammar_text = tokenizer.tokenize(text)
+tokenizer3 = nltk.tokenize.TreebankWordTokenizer()
+grammar_text = tokenizer3.tokenize(text)
+print("grammar tokenization: ")
 print(grammar_text)
+print()
 
