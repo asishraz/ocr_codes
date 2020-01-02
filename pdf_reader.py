f8d2e2de9c17e90d6179f2d00c87dbe22300b8ca
Asish Raz <43290043+asishraz@users.noreply.github.com>
2020-01-02 17:48:37 +0530

    Update pdf_reader.py
    added few lines

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,6 +1,20 @@
+''' opening and extracting the first page of the pdf
+
 import PyPDF2
 pdfFileObj = open('meetingminutes.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 pdfReader.numPages
 pageObj = pdfReader.getPage(0)
 pageObj.extractText()
+
+'''
+
+''' decrypting the encrypted pdf 
+import PyPDF2
+pdfReader = PyPDF2.PdfFileReader(open('encrypted.pdf', 'rb'))
+pdfReader.isEncrypted
+pdfReader.getPage(0)
+pdfReader.decrypt('rosebud')
+pageObj = pdfReader.getPage(0)
+
+'''
