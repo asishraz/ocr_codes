678b3945a6fb9a20cca6d16b3f08169536e7bb0e
Asish Raz <43290043+asishraz@users.noreply.github.com>
2020-01-06 16:55:55 +0530

    Update pytessbase.py
    added few more lines

--- pytessbase.py
+++ pytessbase.py
@@ -5,3 +5,16 @@ with PyTessBaseAPI(path=r"C:\Users\Asish.Asish\Desktop\tesserocr-master\tesseroc
     print(api.GetUTF8Text())
     
     
+# #or else we can do manually 
+# from tesserocr import PyTessBaseAPI
+# api = PyTessBaseAPI(path=r"C:\Users\Asish.Asish\Desktop\tesserocr-master\tesserocr-master\tessdata\tessdata")
+# try:
+#make sure the input file should be in the same directory
+#     api.SetImageFile("Code_snippet.png")
+#     print(api.GetUTF8Text())
+# finally:
+#     print(api.End())
+   
+
+
+    
