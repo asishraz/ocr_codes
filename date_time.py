38e6d4993021b75d099cd5dcbe12790094a9069d
asishraz <iamasish3001@gmail.com>
2020-01-22 20:37:30 +0530

    added this code

--- date_time.py
+++ date_time.py
@@ -37,25 +37,28 @@ import datetime
 def string_taken(str1, str2):
 	year_taken_1 = str1[4:]
 	year_taken_2 = str2[4:]
+	print("year: " +year_taken_1)
 	iterate_1 = iter(str1[0:4])
 	iterate_2 = iter(str2[0:4])
 	slash_join_1 = '/'.join(a+b for a,b in zip(iterate_1,iterate_1))
 	slash_join_2 = '/'.join(a+b for a,b in zip(iterate_2,iterate_2))
+	print("after slash : " +slash_join_1)
+	print("after slash : " +slash_join_2)
 	full_date_1 = slash_join_1 + '/' + year_taken_1
 	full_date_2 = slash_join_2 + '/' + year_taken_2
 	print(full_date_1)
 	print(full_date_2)
 
 
-	date1 = full_date_1
-	date2 = full_date_2
+	# date1 = full_date_1
+	# date2 = full_date_2
 
-	start = datetime.datetime.strptime(date1, '%d/%m/%Y')
-	end = datetime.datetime.strptime(date2, '%d/%m/%Y')
-	step = datetime.timedelta(days=1)
-	while start <= end:
-		print(start.date())
-		start += step 
+	# start = datetime.datetime.strptime(date1, '%d/%m/%Y')
+	# end = datetime.datetime.strptime(date2, '%d/%m/%Y')
+	# step = datetime.timedelta(days=1)
+	# while start <= end:
+	# 	print(start.date())
+	# 	start += step 
 
 
 my_func = string_taken('01042018', '05042018')
