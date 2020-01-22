f9ed9cacdf49821812211414532794cbb6e7d24e
asishraz <iamasish3001@gmail.com>
2020-01-22 20:47:11 +0530

    learned a little about pdb library

--- pdb_usage.py
+++ pdb_usage.py
@@ -10,3 +10,6 @@ if __name__ == "__main_":
 	print("factorial of 3 = ", fact(3))
 
 
+''' for debugging the above code with the use of pdb library'''
+#write the below code at the command prompt to debug the file 'pdb_usage.py'
+#python -m pdb pdb_usage.py
